c410202e5cc9a583b5a5cce6972533b5d20031b0
André Gonçalves <team-web@inm.pt>
2022-06-28 09:21:21 +0100

    updated library

---
+++ src/components.js
@@ -0,0 +1,7 @@
+import teste from '@/library/teste'
+import amountFormat from '@/library/vue-amount-format-input'
+
+export default {
+    teste,
+    amountFormat
+}
---
+++ src/library/teste.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1 @click="handler"> teste </h1>
+</template>
+
+<script setup>
+const handler = () => {
+    console.log('teste handler clicked')
+}
+</script>
